4d604e72dee1ee637a4f357667096de45bb933a4
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-12-01 11:57:48 +0530

    ItemDto Creation

--- src/edu/ijse/mvc/dto/ItemDto.java
+++ src/edu/ijse/mvc/dto/ItemDto.java
@@ -15,13 +15,90 @@ public class ItemDto {
     private String packSize;
     private double unitPrice;
     private int qoh;
-    
-    public void setItemCode(String itemCode){
+
+    public ItemDto() {
+    }
+
+    public ItemDto(String itemCode, String description, String packSize, double unitPrice, int qoh) {
+        this.itemCode = itemCode;
+        this.description = description;
+        this.packSize = packSize;
+        this.unitPrice = unitPrice;
+        this.qoh = qoh;
+    }
+
+    /**
+     * @return the itemCode
+     */
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    /**
+     * @param itemCode the itemCode to set
+     */
+    public void setItemCode(String itemCode) {
         this.itemCode = itemCode;
     }
-    
-    public String getItemCode(){
-        return this.itemCode;
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
     }
-    
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the packSize
+     */
+    public String getPackSize() {
+        return packSize;
+    }
+
+    /**
+     * @param packSize the packSize to set
+     */
+    public void setPackSize(String packSize) {
+        this.packSize = packSize;
+    }
+
+    /**
+     * @return the unitPrice
+     */
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    /**
+     * @param unitPrice the unitPrice to set
+     */
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    /**
+     * @return the qoh
+     */
+    public int getQoh() {
+        return qoh;
+    }
+
+    /**
+     * @param qoh the qoh to set
+     */
+    public void setQoh(int qoh) {
+        this.qoh = qoh;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemDto{" + "itemCode=" + itemCode + ", description=" + description + ", packSize=" + packSize + ", unitPrice=" + unitPrice + ", qoh=" + qoh + '}';
+    }
 }
